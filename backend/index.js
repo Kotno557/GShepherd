0658ee5caec323cc0b611b57a37680b935ba7971
Kotno557 <63412687+Kotno557@users.noreply.github.com>
2022-09-22 15:18:49 +0800

    2022_9_22

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
-const express= require("express");
+const express= require("express"),
+    UserRouter = require('./module/user');
 const app=express();
 const PORT=3000;
-app.get("/",function(req,res){
+app.get("/",function(req,res,next){
     res.send("hello world.");
 });
+
+app.use("/user",UserRouter);
 
 app.listen(PORT,function(err){
     if(err) console.log(err);
