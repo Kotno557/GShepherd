e9fa009a7165b0beeb7952230f61d21141fb29df
Kotno557 <63412687+Kotno557@users.noreply.github.com>
2022-11-08 01:37:18 +0800

    join page

---
+++ Vue/src/components/page/joinPage/joinPage.vue
@@ -0,0 +1,81 @@
+<script>
+    export default { 
+        data(){
+            return {
+               roomId: this.$route.params.id,
+               participantName: ""
+            }
+        },
+        methods: {
+            getParticipantName: function(){
+                console.log('test');
+                this.participantName="temp_name";
+            }
+        },
+        beforeMount(){
+            this.getParticipantName()
+        }
+    }
+</script>
+<template>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..300,0..1,-50..200" />
+
+    <div id="page">
+        <nav>
+            
+            <span class="material-symbols-outlined">
+                <button class="btn">home</button>
+            </span>
+            <div id="band">${Room name}</div>
+            <span class="material-symbols-outlined">
+                <button class="btn">person</button>
+            </span>
+        </nav>
+        <div id="middle">middle</div>
+        <div id="btm"><img src="../../../assets/GShepherd Logo.png"></div>
+    </div>
+</template>
+
+<style scoped>
+@import url('../../../../public/bootstrap-5.2.2/bootstrap-5.2.2-dist/css/bootstrap.css');
+nav{
+    width: 100vm;
+    height: 10vh;
+    background-color: #fff0b3;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+span{
+    margin-left: 10vw;
+    margin-right: 10vw;
+}
+.btn{
+    font-size: 1.8rem;
+}
+nav #band{
+    font-family: 'Noto Sans TC', sans-serif;
+    font-size: 2rem; 
+}
+#page{
+    width: 100vm;
+    height: 100vh;
+}
+#middle{
+    width: 100vm;
+    height: 80vh;
+    background-image: url(./background.png);
+    background-size: cover;
+}
+#btm{
+    width: 100vm;
+    height: 10vh;
+    border-top: 2px rgba(163, 42, 42,0.8) solid;
+    display: flex;
+    align-content: center;    
+    justify-content: center;
+}
+.material-symbols-outlined {
+    font-size: 1.8rem;
+}
+</style>
--- Vue/src/main.js
+++ Vue/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import Gshepherd from './Gshepherd.vue'
 import login from './components/page/login/login_page.vue'
-import display from './components/page/display/display.vue'
+import joinPage from './components/page/joinPage/joinPage.vue'
 import App from './App.vue'
 import dashboard from './components/page/dashboard/dashboard.vue'
 const router = createRouter({
@@ -17,8 +17,8 @@ const router = createRouter({
             component: login
         },
         {
-            path: '/Gshepherd/display/:id',
-            component: display
+            path: '/Gshepherd/join/:id',
+            component: joinPage
         },
         {
             path: '/Gshepherd/dashboard/:id',
