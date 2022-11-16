5e13d4070b8288a6a2fc129740a8527c3f4e135b
Kotno557 <63412687+Kotno557@users.noreply.github.com>
2022-11-16 23:06:00 +0800

    set

--- Vue/vite.config.js
+++ Vue/vite.config.js
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
+  base: process.env.NODE_ENV === 'production' ? '/GShepherd/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
