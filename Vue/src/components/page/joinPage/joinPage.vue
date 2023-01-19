60bbfa6ffdc85f0f3776de47c5dbaa6ba214ac8c
Kotno557 <63412687+Kotno557@users.noreply.github.com>
2023-01-20 02:50:02 +0800

    d

--- Vue/src/components/page/joinPage/joinPage.vue
+++ Vue/src/components/page/joinPage/joinPage.vue
@@ -141,12 +141,13 @@ export default {
         </nav>
         <div id="middle">
             <div id="sheet">
+                {{ pointer }}
                 <select class="form-select form-select-lg" aria-label=".form-select-lg example" v-model="pointer">
                     <option value="-1" disabled selected>請選擇一個活動😀</option>
                     <option v-for="(item, index) in events" :value="index">{{ item.name }}</option>
                 </select>
                 <span v-if="pointer < 0"></span>
-                <vote v-else-if="events[pointer].category === 1" :roomTopic="events[pointer].name" :id="events[pointer].id" :opstions="events[pointer].opstions" />
+                <vote v-else-if="events[pointer].category === 1" :roomTopic="events[pointer].name" :id="events[pointer].id" />
                 <think v-else-if="events[pointer].category === 2" :roomTopic="events[pointer]" />
                 <lotto v-else-if="events[pointer].category === 3" :roomTopic="events[pointer]" />
             </div>
